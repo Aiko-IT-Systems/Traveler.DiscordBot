764c143f14d5d12762be4f7a971383f5a95019a9
Lala Sabathil <aiko@aitsys.dev>
2023-02-20 14:52:51 +0100

    feat: Add modal input for github workflow

--- Commands/GitHubManagement.cs
+++ Commands/GitHubManagement.cs
@@ -4,6 +4,7 @@ using DisCatSharp.ApplicationCommands.Attributes;
 using DisCatSharp.ApplicationCommands.Context;
 using DisCatSharp.Entities;
 using DisCatSharp.Enums;
+using DisCatSharp.Interactivity.Extensions;
 
 using Newtonsoft.Json;
 
@@ -16,8 +17,8 @@ namespace Traveler.DiscordBot.Commands;
 [SlashCommandGroup("github", "GitHub management module", (long)Permissions.Administrator)]
 internal class GitHubManagement : ApplicationCommandsModule
 {
-	[SlashCommand("build", "Build traveler")]
-	internal static async Task BuildAsync(InteractionContext ctx,
+	[SlashCommand("build_fallback", "Build traveler (without modal)")]
+	internal static async Task BuildFallbackAsync(InteractionContext ctx,
 		[Option("version", "Version to build.")] string version,
 		[Option("reference", "Ref to trigger the workflow on. Defaults to 'main'.")] string reference = "main",
 		[Option("build_description", "The build description. Defaults to 'No information given.'.")] string build_description = "No information given",
@@ -48,5 +49,58 @@ internal class GitHubManagement : ApplicationCommandsModule
 		var result = await rest.PostAsync(api_endpoint, string_content);
 		var response = await result.Content.ReadAsStringAsync();
 		await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"{Formatter.MaskedUrl("Workflow", new Uri("https://github.com/Aiko-IT-Systems/Traveler/actions/workflows/steam_deploy.yml"))} trigger success state: {result.IsSuccessStatusCode} ({result.StatusCode})\n\n{response}"));
+	}
+
+	[SlashCommand("build", "Build traveler")]
+	internal static async Task BuildAsync(InteractionContext ctx, [Option("reference", "Ref to trigger the workflow on. Defaults to 'main'.")] string reference = "main")
+	{
+		var modal = BuildWorkflowModal();
+		await ctx.CreateModalResponseAsync(modal);
+		var wait_for_modal = await ctx.Client.GetInteractivity().WaitForModalAsync(modal.CustomId, TimeSpan.FromMinutes(2));
+		if (wait_for_modal.TimedOut)
+		{
+			await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder().WithContent("Modal timed out!").AsEphemeral(true));
+			return;
+		}
+
+		await wait_for_modal.Result.Interaction.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+		var modal_result = wait_for_modal.Result.Interaction.Data.Components.Select(x => new KeyValuePair<string, string>(x.CustomId, x.Value));
+
+		WorkflowExecuteBody github_workflow_data = new(reference, new()
+		{
+			BuildVersion = modal_result.First(x => x.Key == "version").Value,
+			SteamBuildDescription = modal_result.First(x => x.Key == "build_description").Value,
+			SteamBranch = modal_result.First(x => x.Key == "steam_branch").Value,
+			WebsiteUpload = modal_result.First(x => x.Key == "upload_to_website").Value,
+			DiscordAnnounce = modal_result.First(x => x.Key == "announce_on_discord").Value
+		});
+
+		string api_endpoint = "https://api.github.com/repos/Aiko-IT-Systems/Traveler/actions/workflows/steam_deploy.yml/dispatches";
+		HttpClient rest = new();
+		rest.DefaultRequestHeaders.UserAgent.Add(ctx.Client.RestClient.DefaultRequestHeaders.UserAgent.First());
+		rest.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2022-11-28");
+		rest.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+		rest.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Discord.GitHubApiKey);
+		var json = JsonConvert.SerializeObject(github_workflow_data, Formatting.Indented);
+		var string_content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+		var result = await rest.PostAsync(api_endpoint, string_content);
+		var response = await result.Content.ReadAsStringAsync();
+		await wait_for_modal.Result.Interaction.EditOriginalResponseAsync(new DiscordWebhookBuilder().WithContent($"{Formatter.MaskedUrl("Workflow", new Uri("https://github.com/Aiko-IT-Systems/Traveler/actions/workflows/steam_deploy.yml"))} trigger success state: {result.IsSuccessStatusCode} ({result.StatusCode})\n\n{response}"));
+	}
+
+	private static DiscordInteractionModalBuilder BuildWorkflowModal()
+	{
+		DiscordInteractionModalBuilder builder = new("Workflow Inputs");
+		List<DiscordTextComponent> components = new()
+		{
+			new DiscordTextComponent(TextComponentStyle.Small, "version", "Version", "0.X.X.X", 7),
+			new DiscordTextComponent(TextComponentStyle.Paragraph, "build_description", "Description", maxLength: 128, defaultValue: "No description given"),
+			new DiscordTextComponent(TextComponentStyle.Small, "steam_branch", "Steam Branch", defaultValue: "beta"),
+			new DiscordTextComponent(TextComponentStyle.Small, "upload_to_website", "Upload to Website", "true | false", 4, 5, defaultValue: "true"),
+			new DiscordTextComponent(TextComponentStyle.Small, "announce_on_discord", "Annouce on Discord", "true | false", 4, 5, defaultValue: "true")
+		};
+		builder.AddTextComponents(components.ToArray());
+		return builder;
 	}
 }
