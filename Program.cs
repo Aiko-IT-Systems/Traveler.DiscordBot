a2ed203dd67dbbd88511651000311587b6c48a8d
Lala Sabathil <aiko@aitsys.dev>
2023-06-26 23:47:32 +0200

    fix: steam web api key
    Signed-off-by: Lala Sabathil <aiko@aitsys.dev>

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ internal class Program
 		string bot_token = "MTA2OTU3MzkxOTM4NTg2NjI2MA.GzNpq2.6FOqzh-9-GlBKocRqbE9iHkwoe2qp_W-72jw3I";
 
 		string steam_public_web_api_key = "67E5D67118FD2208184467FC7C5461C9";
-		string steam_publisher_web_api_key = "45E67C1BB67D11116F2CAA3CF8606458";
+		string steam_publisher_web_api_key = "DF1A46911A81F005B1FB8B0B4A4574D0";
 		int steam_app_id = 2184270;
 
 		string github_api_key = "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";
